4fa3da7df51a2cbc2b6841821b84fb4c1e0317bd
singhtushar689 <singhtushar689@gmail.com>
2023-01-15 11:27:55 +0530

    First Name

---
+++ Pattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationDetails
+{
+    public class Pattern
+    {
+        public static void FirstName()     //UC1
+        {
+            Console.WriteLine("Enter the First Name ");
+            string firstName = Console.ReadLine();
+            string regexCondition = "^[A-Z]{1}[a-z]{2,}$";
+            Iteration(firstName, regexCondition);
+        }
+        public static void Iteration(string firstName,string regexCondition)
+        {
+            if (Regex.IsMatch(firstName, regexCondition))
+            {
+                Console.WriteLine("Pattern is not Matching! \n Try Again!");
+            }
+            else
+            {
+                Console.WriteLine("Pattern is Matching");
+            }
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UserRegistrationDetails
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Given Option below: \n" +
+                 " 1.Enter the First Name ");   
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option) 
+            {
+                case 1:
+                    Pattern.FirstName();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option ...try again!");
+                    break;
+            
+            }
+        }
+    }
+}
